ba8913c38ef5a96e5a8e897d696014fc808d70a7
Luciano Pereira <hello+world@luciano.io>
2019-07-28 17:41:25 -0400

    first react version

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DragDropUpload from './drag-drop-upload'
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <DragDropUpload></DragDropUpload>
+    </div>
+  );
+}
+
+export default App;
